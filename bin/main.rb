9c6bc7fc99b49026685cdf37f9a4c45c0e249c3d
Alamgir <ourandoc@gmail.com>
2021-01-12 19:18:51 +0500

    fix linters

--- bin/main.rb
+++ bin/main.rb
@@ -38,19 +38,19 @@ while game_start
 
   rounds += 1
 
-  # if any player gets a winning combination, he wins the game otherwise its a tie. break the
-=begin
-  win_combination = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ]
-=end
+  # if any player gets a winning combination.
+  # he wins the game otherwise its a tie. break the loop
+
+  # win_combination = [
+  #   [0, 1, 2],
+  #   [3, 4, 5],
+  #   [6, 7, 8],
+  #   [0, 3, 6],
+  #   [1, 4, 7],
+  #   [2, 5, 8],
+  #   [0, 4, 8],
+  #   [2, 4, 6]
+  # ]
 
 end
 
