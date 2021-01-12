85ab8d4913bfea46d9cd7330c9965be46f2c3cab
Alamgir <ourandoc@gmail.com>
2021-01-12 13:18:35 +0500

    start game planning

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,32 @@
 #!/usr/bin/env ruby
-puts 'Hello!'
+
+class Main
+
+    def initialize
+        @menuOptions
+    end
+
+    def play
+        puts "game started"
+    end
+
+    def invalid_input
+        puts "invalid input"
+        menu
+    end
+    
+    def menu
+        puts "Welcome to Tic-Tac-Toe Game"
+        puts "Press `P` to Play or `Q` to quit"
+        @menuOptions = gets.chomp.downcase
+        if @menuOptions == "p"
+            play
+        elsif @menuOptions == "q"
+            puts "Are you sure: y/n"
+        else
+            invalid_input
+        end
+    end
+end
+
+Main.new.menu
