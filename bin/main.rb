76b2ea6d027ed953dc37d6dcdbe00753ae8ddd32
Leandro-Barretoo <officialdevmi@gmail.com>
2021-01-13 10:12:18 +0000

    milestone 2: hotfix

--- bin/main.rb
+++ bin/main.rb
@@ -15,70 +15,85 @@ puts '#Lets play'
 game_start = true
 rounds = 0
 used_moves = []
+restart = true
 
-while game_start
-  won = false
-  puts rounds
-  puts "#{user_one} Make your move(1-9):"
-  user_move = gets.chomp.to_i
+while restart
+  while game_start
+    won = false
+    draw = false
+    puts rounds
+    puts "#{user_one} Make your move(1-9):"
+    user_move = gets.chomp.to_i
 
-  if user_move.between? 1, 9
+    if user_move.between? 1, 9
 
-    if used_moves.any?(user_move)
-      puts 'Position already taken'
+      if used_moves.any?(user_move)
+        puts 'Position already taken'
+      else
+        used_moves.push(user_move)
+      end
+
+      puts '   | x |   '
+      puts '--- --- ---'
+      puts '   |   |   '
+      puts '--- --- ---'
+      puts '   |   |   '
     else
-      used_moves.push(user_move)
+      puts 'invalid input'
+      puts "#{user_one} Make your move(1-9):"
+      user_move = gets.chomp.to_i
     end
 
-    puts '   |   |   '
-    puts '--- --- ---'
-    puts '   |   |   '
-    puts '--- --- ---'
-    puts '   |   |   '
-  else
-    puts 'invalid input'
-    break
-  end
+    puts "#{user_two} Make your move(1-9):"
+    user_move = gets.chomp.to_i
 
-  puts "#{user_two} Make your move(1-9):"
-  user_move = gets.chomp.to_i
+    if user_move.between? 1, 9
 
-  if user_move.between? 1, 9
+      if used_moves.any?(user_move)
+        puts 'Position already taken'
+      else
+        used_moves.push(user_move)
+      end
 
-    if used_moves.any?(user_move)
-      puts 'Position already taken'
+      puts '   | x |   '
+      puts '--- --- ---'
+      puts '   | o |   '
+      puts '--- --- ---'
+      puts '   |   |   '
     else
-      used_moves.push(user_move)
+      puts 'invalid input'
+      puts "#{user_one} Make your move(1-9):"
+      user_move = gets.chomp.to_i
     end
 
-    puts '   |   |   '
-    puts '--- --- ---'
-    puts '   |   |   '
-    puts '--- --- ---'
-    puts '   |   |   '
-  else
-    puts 'invalid input'
-    break
+    # make move on the board
+    # puts board
+    # initiate next rounds
+
+    # win_combination = [
+    #   [0, 1, 2],
+    #   [3, 4, 5],
+    #   [6, 7, 8],
+    #   [0, 3, 6],
+    #   [1, 4, 7],
+    #   [2, 5, 8],
+    #   [0, 4, 8],
+    #   [2, 4, 6]
+    # ]
+
+    # if any player gets a winning combination.
+
+    if won #if winnig move then true is assign to won and games ends
+      game_start = false
+    end
+
+    if draw #if no winnig move then it triggers the draw variable to be true and the game ends
+      game_start = false
+    end
+
   end
-
-  # make move on the board
-  # puts board
-  # initiate next rounds
-
-  # win_combination = [
-  #   [0, 1, 2],
-  #   [3, 4, 5],
-  #   [6, 7, 8],
-  #   [0, 3, 6],
-  #   [1, 4, 7],
-  #   [2, 5, 8],
-  #   [0, 4, 8],
-  #   [2, 4, 6]
-  # ]
-
-  # if any player gets a winning combination.
-  # he wins the game otherwise its a tie. break the loop
-
-  won == true ? game_start = false : 'Tie the game'
-
+  puts 'winner is -->'
+  puts 'You want to restart: yes or no?'
+  repeat_ans = gets.chomp
+  break unless repeat_ans == 'yes'
 end
