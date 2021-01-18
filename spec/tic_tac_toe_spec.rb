37f0e59ce0e04df9421dcc353f31968a7997f12d
Leandro-Barretoo <officialdevmi@gmail.com>
2021-01-18 14:27:04 +0000

    Fix rspec linter errors

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,6 +1,6 @@
-require '../lib/board.rb'
-require '../lib/player.rb'
-require '../lib/game.rb'
+require_relative '../lib/board.rb'
+require_relative '../lib/player.rb'
+require_relative '../lib/game.rb'
 
 describe Board do
   describe "#placement" do
